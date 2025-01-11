36b5488b64b64930c275486c37f50435a2d5d687
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2025-01-11 17:58:50 +0200

    rolling game is complete

--- Project1.py
+++ Project1.py
@@ -1,9 +1,23 @@
 import random
+
 def roll():
     min_value = 1
     max_value = 6
     roll = random.randint(min_value, max_value)
     return roll
-value = roll()
-print (value)
 
+value = roll()
+
+while True:
+    players = input('Please enter the number of players (1-4): ')
+    
+    if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            break
+        else:
+            print("Must be between 1 and 4.")
+    else:
+        print("Invalid input. Please enter a number.")
+
+print(f"Number of players: {players}")
