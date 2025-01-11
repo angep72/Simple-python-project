d3004e49778bd226692dd4f0792f7dd79f026505
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-11 20:22:03 +0200

    i have succesfully completed my game

--- project2.py
+++ project2.py
@@ -6,5 +6,15 @@ def roll():
     number1= random.randint (min_value, max_value)
     number2= random.randint (min_value, max_value)
     return number1, number2  # return a tuple of two dice values
+def generate_rolling():
+    userInput= input("do you want to roll")
+    while userInput.lower() == "yes":
+        dice1, dice2 = roll()
+        print(f"You rolled: {dice1} and {dice2}")
+        userInput = input("Do you want to roll again?")
+        if userInput.lower() == "no":
+          break
+    print("Thanks for playing!")
 
-print(roll())
+generate_rolling()
+    
