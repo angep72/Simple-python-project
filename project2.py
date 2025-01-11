a045f0c264e6f2b183098538149166d70f5eb747
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-11 20:32:03 +0200

    adding new functionality in my game

--- project2.py
+++ project2.py
@@ -7,14 +7,19 @@ def roll():
     number2= random.randint (min_value, max_value)
     return number1, number2  # return a tuple of two dice values
 def generate_rolling():
-    userInput= input("do you want to roll")
-    while userInput.lower() == "yes":
-        dice1, dice2 = roll()
-        print(f"You rolled: {dice1} and {dice2}")
-        userInput = input("Do you want to roll again?")
-        if userInput.lower() == "no":
-          break
-    print("Thanks for playing!")
+    print("-------------Welcome to the dice rolling game!!!!------------")
+    counts = input("how many times do you want to place the dice")
+    i=0
+    while i < int(counts):
+         userInput= input("do you want to roll")
+         while userInput.lower() == "yes":
+            dice1, dice2 = roll()
+            print(f"You rolled: {dice1} and {dice2}")
+            userInput = input("Do you want to roll again?")
+            if userInput.lower() == "no":
+                break
+            print("Thanks for playing!")
 
+    
 generate_rolling()
     
