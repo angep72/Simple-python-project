e4bb41d550a0ee4d292433293c924e836c614907
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-18 15:03:31 +0200

    adding two different number

--- Guessing_NUmber_game.py
+++ Guessing_NUmber_game.py
@@ -1,17 +1,23 @@
 import random
 def guessGame():
-    guessed_number = 2
+    guessed_number = random.randint(0,10)
     return guessed_number
 
-user_attempts = input("please tell us how many times you want to guess")
+user_attempts = input("-------please tell us how many times you want to guess------------- ")
 
 number = guessGame()
 i=0
-while i< int(user_attempts):
+while i<=int(user_attempts):
     user_input = input("please Guess the succeess number of this game ")
        
     if(int(user_input)== number):
         print ("congratulations you have guessed the number !!!!")
         break
     i = i+1
+    if(i==int(user_attempts)):
+        print("Sorry you have reached the maximum number of attempts. Better luck next time! ")
+        print("The correct number was :", guessGame())
+    break
+
+    
     
