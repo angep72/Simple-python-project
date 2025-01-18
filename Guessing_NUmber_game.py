360ec3ae92f52ca81de97f39243e01b8ee04d82b
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-18 14:50:44 +0200

    this game is the way oof guessing the number of how many games you have implemented

--- Guessing_NUmber_game.py
+++ Guessing_NUmber_game.py
@@ -1,14 +1,17 @@
 import random
 def guessGame():
-    guessed_number = random.randint(1, 100)
+    guessed_number = 2
     return guessed_number
 
-user_input = input("please Guess the succeess number of this game ")
 user_attempts = input("please tell us how many times you want to guess")
 
 number = guessGame()
 i=0
 while i< int(user_attempts):
+    user_input = input("please Guess the succeess number of this game ")
+       
     if(int(user_input)== number):
-        print ("congratulations you have guessed")
+        print ("congratulations you have guessed the number !!!!")
+        break
+    i = i+1
     
