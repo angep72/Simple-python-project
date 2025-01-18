ea44342b97c8f5d49d0ce1166497fa4a66ee3a43
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-18 13:58:42 +0200

    designing a simple calcultor

--- addition.py
+++ addition.py
@@ -1,11 +1,8 @@
 def additon(a,b):
  return a + b
-total = additon(3,4)
-print (total)
 
 def substract (a,b):
     return a - b
 
-total = substract(5,3)
-print (total)
+const first number = input("please enter a number ")
 
