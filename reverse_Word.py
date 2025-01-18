16fc7290c299663ac6cf6db8a67d98bc4af2e583
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-18 15:31:58 +0200

    different methods on list

--- reverse_Word.py
+++ reverse_Word.py
@@ -1,1 +1,4 @@
 # Examples of important string methods in python
+#Python list methods are built-in functions that allow us to perform various operations on lists, 
+# such as adding, removing, or modifying elements. 
+# In this article, we’ll explore all Python list methods with a simple example.
