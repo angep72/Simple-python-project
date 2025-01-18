3bc25ce3e58b3bc65c83c7e776d2dc28badd53e8
Pauline Umunyana <p.umunyana@alustudent.com>
2025-01-18 14:39:32 +0200

    adding new functionality in guessing the number

--- Guessing_NUmber_game.py
+++ Guessing_NUmber_game.py
@@ -2,5 +2,10 @@ import random
 def guessGame():
     guessed_number = random.randint(1, 100)
     return guessed_number
-user_input = input("please Guess the succeess number of this game")
+
+user_input = input("please Guess the succeess number of this game ")
+
+number = guessGame()
+if(number == int(user_input)):
+
     
