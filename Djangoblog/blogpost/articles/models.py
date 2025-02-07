c8d1b5d95004ef1af4ed4143c292c042b5b570f1
angep73 <p.umunyana@alustudent.com>
2025-02-07 14:35:05 +0200

    adding different migrations

--- Djangoblog/blogpost/articles/models.py
+++ Djangoblog/blogpost/articles/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField
-    body = models.TextFiel
+    body = models.TextField
     date = models.DateTimeField(auto_now_add=True)
     
