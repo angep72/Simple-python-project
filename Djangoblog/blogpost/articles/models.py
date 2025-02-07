253f1a638a42d2389d68fc7bf846450d74bce35e
angep73 <p.umunyana@alustudent.com>
2025-02-07 14:26:28 +0200

    adding different models

--- Djangoblog/blogpost/articles/models.py
+++ Djangoblog/blogpost/articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField
+    body = models.TextFiel
+    date = models.DateTimeField(auto_now_add=True)
+    
