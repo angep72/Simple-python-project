84ba3f7460741c4481d21d461fd9b8557d2abeae
angep73 <p.umunyana@alustudent.com>
2025-04-12 15:35:42 +0200

    This is the 26th challenge

--- common_elements.py
+++ common_elements.py
@@ -1,5 +1,8 @@
 #Write a function that returns the common elements between two lists.
 def my_function(list1, list2):
-    joined_list = list1 + list2
-    return joined_list
-print(my_function([1, 2, 3,4,4], [4, 5, 6]))
+    result = []
+    for item in list1:
+        for items in list2:
+            if item == items:
+                result.append(items)
+    return result
