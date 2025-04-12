d46e240ac3bb22d62c8bdc2ac25ea0aa75f26cd1
angep73 <p.umunyana@alustudent.com>
2025-04-12 16:21:01 +0200

    This is 30th challenge

---
+++ enumerate.py
@@ -0,0 +1,4 @@
+#Use enumerate() to print the index and value of each list element.
+def enumerate_function(list_numbers):
+    for index,value in enumerate(list_numbers):
+        print(index, value)
