0cc2ff681476c7a1e1859512f12712331f9ce720
angep73 <p.umunyana@alustudent.com>
2025-04-14 16:00:01 +0200

    This is my 43rd challenge

---
+++ merge_function.py
@@ -0,0 +1,15 @@
+devBio = {
+  "name": "Ihechikara",
+  "age": 500,
+  "language": "Python"
+}
+
+tools = {
+  "dev environment": "JupyterLab",
+  "os": "Windows",
+  "visualization": "Matplotlib"
+}
+
+devBio.update(tools)
+
+print(devBio)
