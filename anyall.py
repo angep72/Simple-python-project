b1dff64c6ab5287ce80c6677f3bfcffbcbc09908
angep73 <p.umunyana@alustudent.com>
2025-04-14 15:29:13 +0200

    adding 38th challenge

---
+++ anyall.py
@@ -0,0 +1,13 @@
+# Use any() and all() on a list of booleans.
+
+bools = [True, True, False, True]
+
+# any() returns True if at least one element is True
+print(any(bools))  # Output: True
+
+# all() returns True only if all elements are True
+print(all(bools))  # Output: False
+#Quick Explanation:
+# any(bools) → checks if any value in the list is True
+#all(bools) → checks if all values are True
+
