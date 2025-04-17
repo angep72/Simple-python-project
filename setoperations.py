e7cee63d561d3c67551def6a9f3b6c2db4a439d8
angep73 <p.umunyana@alustudent.com>
2025-04-17 11:40:42 +0200

    This is 49 th challenge

---
+++ setoperations.py
@@ -0,0 +1,11 @@
+# Use set operations: union, intersection, difference.
+
+setA = {"a","b","c","d","e","f","g","h"}
+setB = {"g","h","i","j","k","l","m","a"}
+
+intersection = setA.intersection(setB)
+print(intersection)
+union_set = setA.union(setB)
+print(union_set)
+difference = setA.difference(setB)
+print(difference)
