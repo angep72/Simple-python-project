370f2f14e5163380c5076fc4369a2d7ee9412477
angep73 <p.umunyana@alustudent.com>
2025-04-18 10:18:53 +0200

    This is my 69th Challenge

---
+++ passwordguesse.py
@@ -0,0 +1,11 @@
+# Write a function that returns a random password using random and string.
+import random
+
+
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
